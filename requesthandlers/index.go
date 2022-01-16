4ab42a50bb56b5cd024c09e33d8088d263912313
Kevin Liao <me@liaokev.in>
2022-01-16 13:44:02 -0800

    Make redirect to custom event name in JS vs. Golang

--- requesthandlers/index.go
+++ requesthandlers/index.go
@@ -1,8 +1,9 @@
 package requesthandlers
 
 import (
-	"fmt"
+	"log"
 	"net/http"
+	"text/template"
 
 	"github.com/google/uuid"
 	"github.com/julienschmidt/httprouter"
@@ -14,9 +15,14 @@ type IndexData struct {
 }
 
 func IndexHandler(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	t := template.Must(template.ParseFiles("templates/index.html"))
 	if r.URL.Path != "/" {
 		http.NotFound(w, r)
 		return
 	}
-	http.Redirect(w, r, fmt.Sprintf("/event/%s", uuid.New().String()), http.StatusMovedPermanently)
+	uuid := uuid.New().String()
+	err := t.Execute(w, IndexData{Uuid: uuid})
+	if err != nil {
+		log.Println("Failed to parse template.")
+	}
 }
