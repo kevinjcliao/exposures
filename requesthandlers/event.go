d3e4972c88546b9a721c59fb9e48d9b79ee7ee8e
Kevin Liao <me@liaokev.in>
2022-01-16 12:45:07 -0800

    Unbreak lint.

--- requesthandlers/event.go
+++ requesthandlers/event.go
@@ -15,7 +15,10 @@ func EventHandler(w http.ResponseWriter, r *http.Request, ps httprouter.Params) 
 
 	if _, err := uuid.Parse(id); err != nil {
 		t = template.Must(template.ParseFiles("templates/404.html"))
-		t.Execute(w, IndexData{Uuid: id})
+		err := t.Execute(w, IndexData{Uuid: id})
+		if err != nil {
+			log.Println("Failed to parse 404 template.")
+		}
 		return
 	}
 	err := t.Execute(w, IndexData{Uuid: id})
